ae84df75dc9556a0c7cf586f5857b6a2988db6fa
Julia Marin Salas <jul.ms1193@gmail.com>
2026-05-18 19:11:02 +0200

    Deploy only GigantePage classification

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
-import { lazy, Suspense } from 'react';
-import HomePage from './pages/HomePage';
-
-const GigantePage = lazy(() => import('./pages/GigantePage'));
+import GigantePage from './pages/GigantePage';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<div className="min-h-screen bg-gray-950" />}>
-        <Routes>
-          <Route path="/"        element={<HomePage />} />
-          <Route path="/gigante" element={<GigantePage />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  );
+  return <GigantePage />;
 }
